36dc565d108efe88f3d342f7876ab7b839d37e2b
Akemi <wakamihirro@gmail.com>
2025-04-20 10:59:24 +0700

    First

---
+++ app/movie/[id].tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MovieDetail = () => {
+  return (
+    <View>
+      <Text> MovieDetail</Text>
+    </View>
+  )
+}
+
+export default MovieDetail
+
+const styles = StyleSheet.create({})
