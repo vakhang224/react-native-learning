7d321c2a05d6b4dcf03f06d836002a75239cb296
Akemi <wakamihirro@gmail.com>
2025-04-24 00:26:44 +0700

    im stupid

--- services/api.ts
+++ services/api.ts
@@ -11,7 +11,7 @@ export const fetchMovies = async({ query }: {query: string}) => {
 
     const endpoint = query
     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` 
-    : `${TMDB_CONFIG.BASE_URL}/discorver/movie?sort_by=popularity.desc`;
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
     const response = await fetch(endpoint, {
         method: 'GET',
         headers: TMDB_CONFIG.headers
